c748a38a8ecbf8450371bc5d4f42f8157a921479
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-09 20:00:35 +0200

    implement movie details page with data fetching and navigation links

--- src/pages/movieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/movieDetailsPage/MovieDetailsPage.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Link, Outlet, useLocation, useParams } from "react-router-dom";
+import fetchData from "../../tmdb-api";
 
 const MovieDetailsPage = () => {
-	return <div>MovieDetailsPage</div>;
+	const [details, setDetails] = useState({});
+	const { movieId } = useParams();
+	const location = useLocation();
+
+	useEffect(() => {
+		async function fetchDetail() {
+			const result = await fetchData("details", { movieId });
+			setDetails(result);
+		}
+
+		fetchDetail();
+	}, [movieId]);
+
+	return (
+		<div>
+			<Link to={location.state ?? "/"}>Go back</Link>
+			{details && (
+				<div>
+					<img
+						src={`https://image.tmdb.org/t/p/w500${details.poster_path}`}
+						alt={`${details.title} movie poster`}
+					/>
+					<h1>{details.title}</h1>
+					<p>User score: {details.popularity}</p>
+					<p>{details.overview}</p>
+					<p>
+						{details.genres?.length > 0
+							? details.genres
+									.map((genre) => genre.name)
+									.join(", ")
+							: "No genres available."}
+					</p>
+					<h2>Additional information</h2>
+					<ul>
+						<li>
+							<Link to="cast">Cast</Link>
+						</li>
+						<li>
+							<Link to="reviews">Reviews</Link>
+						</li>
+					</ul>
+					<Outlet context={movieId} />
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default MovieDetailsPage;

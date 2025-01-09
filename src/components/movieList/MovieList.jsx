665a8810a3598be5f209c8f295738316572ecffd
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-09 20:00:20 +0200

    implement movie list rendering with links to movie details

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
-const MovieList = () => {
-	return <div>MovieList</div>;
+const MovieList = ({ movies }) => {
+	const location = useLocation();
+	return (
+		<ul>
+			{movies.map((movie) => {
+				return (
+					<li key={movie.id}>
+						<Link to={`/movies/${movie.id}`} state={location}>
+							{movie.title}
+						</Link>
+					</li>
+				);
+			})}
+		</ul>
+	);
 };
 
 export default MovieList;

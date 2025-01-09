fd38375c450f6732a8263a421120902518229b5b
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-09 20:00:14 +0200

    fetch movie cast data and display in MovieCast component

--- src/components/movieCast/MovieCast.jsx
+++ src/components/movieCast/MovieCast.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import fetchData from "../../tmdb-api";
 
 const MovieCast = () => {
-	return <div>MovieCast</div>;
+	const [cast, setCast] = useState([]);
+	const movieId = useOutletContext();
+
+	useEffect(() => {
+		async function fetchCast() {
+			const result = await fetchData("credits", { movieId });
+			setCast(result.cast);
+		}
+
+		fetchCast();
+	}, [movieId]);
+
+	return (
+		<ul>
+			{cast &&
+				cast.map((member) => {
+					return (
+						<li key={member.id}>
+							<img
+								src={`https://image.tmdb.org/t/p/w185${member.profile_path}`}
+								alt={`${member.name}`}
+							/>
+							<p>{member.name}</p>
+							<p>Character: {member.character}</p>
+						</li>
+					);
+				})}
+		</ul>
+	);
 };
 
 export default MovieCast;

09facee8ebe6037e71c9005d8240ea54a44fb747
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-09 20:00:06 +0200

    refactor App component to use lazy loading for pages and update routes

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,19 +1,30 @@
-import Navigation from "../navigation";
+import Navigation from "../navigation/Navigation";
 import { Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
+
+const HomePage = lazy(() => import("../../pages/homePage/HomePage"));
+const MoviesPage = lazy(() => import("../../pages/moviesPage/MoviesPage"));
+const MovieDetailsPage = lazy(() =>
+	import("../../pages/movieDetailsPage/MovieDetailsPage")
+);
+const MovieCast = lazy(() => import("../movieCast/MovieCast"));
+const MovieReviews = lazy(() => import("../movieReviews/MovieReviews"));
+const NotFoundPage = lazy(() =>
+	import("../../pages/notFoundPage/NotFoundPage")
+);
 
 const App = () => {
 	return (
 		<Suspense fallback={<p>Loading...</p>}>
 			<Navigation />
 			<Routes>
-				<Route path="/" element={""} />
-				<Route path="/movies" element={""} />
-				<Route path="/movies/:movieId" element={""}>
-					<Route path="cast" element={""} />
-					<Route path="reviews" element={""} />
+				<Route path="/" element={<HomePage />} />
+				<Route path="/movies" element={<MoviesPage />} />
+				<Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+					<Route path="cast" element={<MovieCast />} />
+					<Route path="reviews" element={<MovieReviews />} />
 				</Route>
-				<Route path="*" element={""} />
+				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 		</Suspense>
 	);

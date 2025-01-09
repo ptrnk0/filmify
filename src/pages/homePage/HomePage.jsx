1fe3657db7dca4fc779564be013d1699c60fda19
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-09 20:00:30 +0200

    fetch trending movies and render them in MovieList component

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import MovieList from "../../components/movieList/MovieList";
+import fetchData from "../../tmdb-api";
 
 const HomePage = () => {
-	return <div>HomePage</div>;
+	const [movies, setMovies] = useState([]);
+
+	useEffect(() => {
+		async function fetchTrending() {
+			setMovies([]);
+			const { results } = await fetchData("trending");
+			setMovies(results);
+		}
+
+		fetchTrending();
+	}, []);
+
+	return <MovieList movies={movies} />;
 };
 
 export default HomePage;

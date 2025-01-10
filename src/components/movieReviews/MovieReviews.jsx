17a444ee264e47579c356be3b6f98749d99c29ee
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-10 13:05:13 +0200

    implement MovieReviews component with data fetching and HTML tag removal

--- src/components/movieReviews/MovieReviews.jsx
+++ src/components/movieReviews/MovieReviews.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import useFetchData from "../../hooks/useFetchData";
+import { useOutletContext } from "react-router-dom";
 
 const MovieReviews = () => {
-	return <div>MovieReviews</div>;
+	const movieId = useOutletContext();
+
+	const removeHtmlTags = (text) => {
+		return text.replace(/<[^>]*>/g, "");
+	};
+
+	const {
+		data: { results },
+		loading,
+		error,
+	} = useFetchData("reviews", movieId);
+
+	return (
+		<ul>
+			{results &&
+				results.map((review) => {
+					return (
+						<li key={review.id}>
+							<article>
+								<h2>Author: {review.author}</h2>
+								<p>{removeHtmlTags(review.content)}</p>
+							</article>
+						</li>
+					);
+				})}
+		</ul>
+	);
 };
 
 export default MovieReviews;

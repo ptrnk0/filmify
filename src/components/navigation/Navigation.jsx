b222b9189c6a2617a85e3c582363291f99ab0da1
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-10 20:28:25 +0200

    refactor Navigation component to apply dynamic link styling using CSS modules

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,10 +1,20 @@
+import clsx from "clsx";
 import { NavLink } from "react-router-dom";
+import css from "./Navigation.module.css";
 
 const Navigation = () => {
+	const buildLinkClass = ({ isActive }) => {
+		return clsx(css.link, isActive && css.active);
+	};
+
 	return (
 		<nav>
-			<NavLink to="/">Home</NavLink>
-			<NavLink to="/movies">Movies</NavLink>
+			<NavLink to="/" className={buildLinkClass}>
+				Home
+			</NavLink>
+			<NavLink to="/movies" className={buildLinkClass}>
+				Movies
+			</NavLink>
 		</nav>
 	);
 };

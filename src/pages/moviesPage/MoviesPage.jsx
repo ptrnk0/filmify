74b7545d340da6849f0fc0a2e978230a23400627
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-10 20:22:51 +0200

    refactor MoviesPage component to implement search functionality and handle loading and error states

--- src/pages/moviesPage/MoviesPage.jsx
+++ src/pages/moviesPage/MoviesPage.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieList from "../../components/movieList/MovieList";
+import useFetchData from "../../hooks/useFetchData";
 
 const MoviesPage = () => {
-	return <div>MoviesPage</div>;
+	const [searchParams, setSearchParams] = useSearchParams();
+	const query = searchParams.get("query");
+
+	const { data, loading, error } = useFetchData("search", query);
+
+	const handleSubmit = (evt) => {
+		evt.preventDefault();
+		const form = evt.target;
+		setSearchParams({ query: form.elements.search.value });
+		form.reset();
+	};
+
+	return (
+		<>
+			<form onSubmit={(evt) => handleSubmit(evt)}>
+				<input type="text" name="search" />
+				<button type="submit">Search</button>
+			</form>
+			{loading && <p>Loading...</p>}
+			{error && <p>Something went wrong, please try again.</p>}
+			<>{data && <MovieList movies={data.results} />}</>
+		</>
+	);
 };
 
 export default MoviesPage;

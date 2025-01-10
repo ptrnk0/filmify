fb8007265889351f42baa0ab3da409f3a52d1da9
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-10 20:22:18 +0200

    refactor MovieCast component to handle loading and error states

--- src/components/movieCast/MovieCast.jsx
+++ src/components/movieCast/MovieCast.jsx
@@ -4,28 +4,28 @@ import { useOutletContext } from "react-router-dom";
 const MovieCast = () => {
 	const movieId = useOutletContext();
 
-	const {
-		data: { cast },
-		loading,
-		error,
-	} = useFetchData("credits", movieId);
+	const { data, loading, error } = useFetchData("credits", movieId);
 
 	return (
-		<ul>
-			{cast &&
-				cast.map((member) => {
-					return (
-						<li key={member.id}>
-							<img
-								src={`https://image.tmdb.org/t/p/w185${member.profile_path}`}
-								alt={`${member.name}`}
-							/>
-							<p>{member.name}</p>
-							<p>Character: {member.character}</p>
-						</li>
-					);
-				})}
-		</ul>
+		<>
+			{loading && <p>Loading...</p>}
+			{error && <p>Something went wrong, please try again.</p>}
+			<ul>
+				{data &&
+					data.cast.map((member) => {
+						return (
+							<li key={member.id}>
+								<img
+									src={`https://image.tmdb.org/t/p/w185${member.profile_path}`}
+									alt={`${member.name}`}
+								/>
+								<p>{member.name}</p>
+								<p>Character: {member.character}</p>
+							</li>
+						);
+					})}
+			</ul>
+		</>
 	);
 };
 

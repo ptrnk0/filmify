944ac8cb1fde0d5fb642a7ed7afe011867c05140
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-10 20:22:23 +0200

    refactor MovieReviews component to handle loading and error states

--- src/components/movieReviews/MovieReviews.jsx
+++ src/components/movieReviews/MovieReviews.jsx
@@ -8,29 +8,30 @@ const MovieReviews = () => {
 		return text.replace(/<[^>]*>/g, "");
 	};
 
-	const {
-		data: { results },
-		loading,
-		error,
-	} = useFetchData("reviews", movieId);
+	const { data, loading, error } = useFetchData("reviews", movieId);
 
 	return (
-		<ul>
-			{Array.isArray(results) && results.length ? (
-				results.map((review) => {
-					return (
-						<li key={review.id}>
-							<article>
-								<h2>Author: {review.author}</h2>
-								<p>{removeHtmlTags(review.content)}</p>
-							</article>
-						</li>
-					);
-				})
-			) : (
-				<p>We don&apos;t have any reviews for this movie.</p>
-			)}
-		</ul>
+		<>
+			{loading && <p>Loading...</p>}
+			{error && <p>Something went wrong, please try again.</p>}
+			<ul>
+				{data &&
+					(data.results.length > 0 ? (
+						data.results.map((review) => {
+							return (
+								<li key={review.id}>
+									<article>
+										<h2>Author: {review.author}</h2>
+										<p>{removeHtmlTags(review.content)}</p>
+									</article>
+								</li>
+							);
+						})
+					) : (
+						<p>We don&apos;t have any reviews for this movie.</p>
+					))}
+			</ul>
+		</>
 	);
 };
 

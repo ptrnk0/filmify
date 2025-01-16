f714b46d5668b49bda03f3089e9dad7e9c8c926e
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-16 16:18:30 +0200

    removing CSS module

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,10 @@
 import { Link } from "react-router-dom";
 import Navigation from "../navigation/Navigation";
-import css from "./Header.module.css";
 
 const Header = () => {
 	return (
 		<header>
-			<Link to="/" className={css.logo}>
-				Filmify
-			</Link>
+			<Link to="/">Filmify</Link>
 			<Navigation />
 		</header>
 	);

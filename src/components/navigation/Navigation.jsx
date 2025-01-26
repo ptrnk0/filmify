82528fe03e2a7f876c2e11d70d503912abc7ea41
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-26 19:17:47 +0200

    style: refactor Navigation component to include LocalsBtn and improve layout

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,11 +1,11 @@
 import clsx from "clsx";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import css from "./Navigation.module.css";
 import { useTranslation } from "react-i18next";
-import LOCALS from "../../i18n/constants";
+import LocalsBtn from "../localsBtn/LocalsBtn";
 
 const Navigation = () => {
-	const [t, i18n] = useTranslation();
+	const [t] = useTranslation();
 
 	const buildLinkClass = ({ isActive }) => {
 		return clsx(css.link, isActive && css.active);
@@ -13,32 +13,24 @@ const Navigation = () => {
 
 	return (
 		<>
-			<nav className={css.navLinkContainer}>
-				<NavLink to="/" className={buildLinkClass}>
-					{t("Trending")}
-				</NavLink>
-				<NavLink to="/movies" className={buildLinkClass}>
-					{t("Movie")}
-				</NavLink>
-			</nav>
-			<div className={css.localsBtnContainer}>
-				<button
-					onClick={() => {
-						i18n.changeLanguage(LOCALS.EN);
-					}}
-					className={css.localsBtn}
-				>
-					EN
-				</button>
-				<button
-					onClick={() => {
-						i18n.changeLanguage(LOCALS.UK);
-					}}
-					className={css.localsBtn}
-				>
-					UA
-				</button>
+			<div className={css.logoContainer}>
+				<div className="container">
+					<Link to="/" className={css.headerLogo}>
+						Filmify
+					</Link>
+				</div>
 			</div>
+			<header className={`container ${css.header}`}>
+				<nav className={css.navLinkContainer}>
+					<NavLink to="/" className={buildLinkClass}>
+						{t("Trending")}
+					</NavLink>
+					<NavLink to="/movies" className={buildLinkClass}>
+						{t("Movie")}
+					</NavLink>
+				</nav>
+				<LocalsBtn />
+			</header>
 		</>
 	);
 };

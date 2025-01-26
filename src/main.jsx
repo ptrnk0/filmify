3657da5feebb9b4116b9e14fb2667ff35e12b1f4
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-26 18:24:33 +0200

    refactor: remove old CSS file and update import paths in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "modern-normalize";
-import "./index.css";
 import App from "./components/app/App";
 import "./i18n/index";
+import "modern-normalize";
+import "./styles/index.css";
+import "./styles/common.css";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>

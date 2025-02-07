8a248a276f162949bec6f1a585f1a9b6a6744ceb
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-07 18:25:49 +0200

    style: enhance MovieList component with CSS modules for improved styling

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,13 +1,20 @@
 import { Link, useLocation } from "react-router-dom";
+import css from "./MovieList.module.css";
 
 const MovieList = ({ movies }) => {
 	const location = useLocation();
 	return (
-		<ul>
+		<ul className={css.movieList}>
 			{movies.map((movie) => {
 				return (
-					<li key={movie.id}>
+					<li key={movie.id} className={css.movieListItem}>
 						<Link to={`/movies/${movie.id}`} state={location}>
+							<div className={css.movieImgContainer}>
+								<img
+									src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+									alt={`${movie.title}`}
+								/>
+							</div>
 							{movie.title}
 						</Link>
 					</li>

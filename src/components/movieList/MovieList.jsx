e453d50ebd2388f0160c7cc5ce0fffcad3051d27
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-08 12:35:13 +0200

    refactor: remove unused useLocation hook from MovieList component

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,14 +1,13 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import css from "./MovieList.module.css";
 
 const MovieList = ({ movies }) => {
-	const location = useLocation();
 	return (
 		<ul className={css.movieList}>
 			{movies.map((movie) => {
 				return (
 					<li key={movie.id} className={css.movieListItem}>
-						<Link to={`/movies/${movie.id}`} state={location}>
+						<Link to={`/movies/${movie.id}`}>
 							<div className={css.movieImgContainer}>
 								<img
 									src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}

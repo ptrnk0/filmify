8e1ea4d4280bc4686d97b118b8a11743bcd74dbd
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-08 12:36:50 +0200

    i18n: integrate translations for movie details and update overview display in MovieDetailsPage component

--- src/pages/movieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/movieDetailsPage/MovieDetailsPage.jsx
@@ -1,17 +1,15 @@
-import { useRef } from "react";
 import useFetchData from "../../hooks/useFetchData";
-import { Link, Outlet, useLocation, useParams } from "react-router-dom";
+import { Link, Outlet, useParams } from "react-router-dom";
 import css from "./MovieDetailsPage.module.css";
+import { useTranslation } from "react-i18next";
 
 const MovieDetailsPage = () => {
-	const location = useLocation();
-	const refLocation = useRef(location.state);
+	const [t] = useTranslation();
 	const { movieId } = useParams();
 	const { data, loading, error } = useFetchData("details", movieId);
 
 	return (
 		<section>
-			<Link to={refLocation.current ?? "/"}>Go back</Link>
 			{loading && <p>Loading...</p>}
 			{error && <p>Something went wrong, please try again.</p>}
 			{data && (
@@ -34,28 +32,21 @@ const MovieDetailsPage = () => {
 						</div>
 					</div>
 					<h1>{data.title}</h1>
-					<p>User score: {data.vote_average}</p>
-					<p>Overview: {data.overview}</p>
 					<p>
-						Genres:{" "}
-						{data.genres?.length > 0
-							? data.genres.map((genre) => genre.name).join(", ")
-							: "No genres available."}
+						{data.genres &&
+							data.genres.map((genre) => genre.name).join(", ")}
 					</p>
-					<h2>Additional information</h2>
+					<p>User score: {data.vote_average}</p>
+					<p>
+						{t("Overview")}: {data.overview}
+					</p>
+					<h2>{t("Additional")}</h2>
 					<ul>
 						<li>
-							<Link to="cast" state={refLocation.current ?? "/"}>
-								Cast
-							</Link>
+							<Link to="cast">{t("Cast")}</Link>
 						</li>
 						<li>
-							<Link
-								to="reviews"
-								state={refLocation.current ?? "/"}
-							>
-								Reviews
-							</Link>
+							<Link to="reviews">{t("Reviews")}</Link>
 						</li>
 					</ul>
 					<Outlet context={movieId} />

fca06a256ea55086454fff09359b115179cd085a
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-10 19:34:13 +0200

    feat: enhance Layout component with CSS for improved structure and styling

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,19 +1,21 @@
 import { Outlet } from "react-router-dom";
 
+import css from "./Layout.module.css";
+
 import Header from "../header/Header";
 import Logo from "../logo/Logo";
 import Footer from "../footer/Footer";
 
 const Navigation = () => {
 	return (
-		<>
+		<div className={css.pageContainer}>
 			<Logo />
 			<Header />
 			<main className="container">
 				<Outlet />
 			</main>
 			<Footer />
-		</>
+		</div>
 	);
 };
 

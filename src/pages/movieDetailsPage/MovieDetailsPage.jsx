e9e692867e5b54b725229ac23b26da784f20086e
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-12 19:39:59 +0200

    feat: enhance MovieDetailsPage with dynamic background color and production companies display

--- src/pages/movieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/movieDetailsPage/MovieDetailsPage.jsx
@@ -4,24 +4,39 @@ import { useTranslation } from "react-i18next";
 import css from "./MovieDetailsPage.module.css";
 
 import useFetchData from "../../hooks/useFetchData";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import MovieDetails from "../../components/movieDetails/MovieDetails";
 import FadeContent from "../../components/FadeContent";
+import { setBgColor, getPosterColor } from "../../components/bgColor";
 
 const MovieDetailsPage = () => {
 	const [t] = useTranslation();
 	const { movieId } = useParams();
 	const { data, loading, error } = useFetchData("details", movieId);
 	const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+	const [mainColor, setMainColor] = useState("white");
 
 	useEffect(() => {
 		const handleResize = () => {
 			setIsMobile(window.innerWidth < 768);
 		};
 
+		(async () => {
+			setMainColor(
+				await getPosterColor(
+					`https://image.tmdb.org/t/p/w300${data?.poster_path}`
+				)
+			);
+		})();
+
+		setBgColor(mainColor);
+
 		window.addEventListener("resize", handleResize);
-		return () => window.removeEventListener("resize", handleResize);
-	}, []);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+			setBgColor();
+		};
+	}, [data, mainColor]);
 
 	return (
 		<section>
@@ -39,6 +54,9 @@ const MovieDetailsPage = () => {
 							>
 								<div
 									className={css.posterBackdropGradient}
+									style={{
+										backgroundImage: `linear-gradient(to right, rgb(${mainColor[0]}, ${mainColor[1]}, ${mainColor[2]}) 20%, rgba(10.5, 31.5, 31.5, 0) 50%)`,
+									}}
 								></div>
 								<div className={css.posterDetail}>
 									<div className={css.posterImgContainer}>
@@ -46,6 +64,7 @@ const MovieDetailsPage = () => {
 											src={`https://image.tmdb.org/t/p/w300${data.poster_path}`}
 											alt={`${data.title} movie poster`}
 											className={css.posterImg}
+											id="img"
 										/>
 									</div>
 									<div className={css.posterText}>
@@ -57,6 +76,22 @@ const MovieDetailsPage = () => {
 							</div>
 						</div>
 						{isMobile && <MovieDetails data={data} />}
+						<div className={css.productionCompanies}>
+							{data.production_companies.map(
+								({ id, logo_path }) => {
+									if (logo_path) {
+										return (
+											<div key={id}>
+												<img
+													src={`https://image.tmdb.org/t/p/w300${logo_path}`}
+													alt=""
+												/>
+											</div>
+										);
+									}
+								}
+							)}
+						</div>
 						<h2>{t("Additional")}</h2>
 						<ul>
 							<li>

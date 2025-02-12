29269c2a3813ec8dfde0fd62dc7a51e2c33f1f1d
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-12 10:17:55 +0200

    feat: enhance MovieDetails component with CSS for improved styling and layout

--- src/components/movieDetails/MovieDetails.jsx
+++ src/components/movieDetails/MovieDetails.jsx
@@ -1,17 +1,21 @@
 import { useTranslation } from "react-i18next";
 
+import css from "./MovieDetails.module.css";
+
 const MovieDetails = ({ data }) => {
 	const [t] = useTranslation();
 
 	return (
-		<div>
-			<h1>{data.title}</h1>
-			<p>
+		<div className={css.movieDetailsContainer}>
+			<h1 className={css.movieTitle}>{data.title}</h1>
+			<p className={css.movieGenres}>
 				{data.genres &&
 					data.genres.map((genre) => genre.name).join(", ")}
 			</p>
-			<p>User score: {data.vote_average}</p>
-			<p>
+			<p className={css.movieUserScore}>
+				User score: {data.vote_average}
+			</p>
+			<p className={css.movieOverview}>
 				{t("Overview")}: {data.overview}
 			</p>
 		</div>

a4e36b1f8d7e4df4649a12cb0a2967637f8bfe87
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-20 19:55:01 +0200

    feat: implement responsive image handling with picture element in MovieDetailsPage

--- src/pages/movieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/movieDetailsPage/MovieDetailsPage.jsx
@@ -10,74 +10,75 @@ import FadeContent from "../../components/FadeContent";
 import ProductionCompaniesList from "../../components/productionCompaniesList/ProductionCompaniesList";
 
 const MovieDetailsPage = () => {
-	const [t] = useTranslation();
-	const { movieId } = useParams();
-	const { data, loading, error } = useFetchData("details", movieId);
-	const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [t] = useTranslation();
+  const { movieId } = useParams();
+  const { data, loading, error } = useFetchData("details", movieId);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
-	useEffect(() => {
-		const handleResize = () => {
-			setIsMobile(window.innerWidth < 768);
-		};
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
 
-		window.addEventListener("resize", handleResize);
-		return () => {
-			window.removeEventListener("resize", handleResize);
-		};
-	}, []);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-	return (
-		<section>
-			<FadeContent>
-				{loading && <p>Loading...</p>}
-				{error && <p>Something went wrong, please try again.</p>}
-				{data && (
-					<>
-						<div className={css.poster}>
-							<div
-								className={css.posterBackdrop}
-								style={{
-									backgroundImage: `url(https://image.tmdb.org/t/p/w1280${data.backdrop_path})`,
-								}}
-							>
-								<div
-									className={css.posterBackdropGradient}
-								></div>
-								<div className={css.posterDetail}>
-									<div className={css.posterImgContainer}>
-										<img
-											src={`https://image.tmdb.org/t/p/w300${data.poster_path}`}
-											alt={`${data.title} movie poster`}
-											className={css.posterImg}
-										/>
-									</div>
-									<div className={css.posterText}>
-										{!isMobile && (
-											<MovieDetails data={data} />
-										)}
-									</div>
-								</div>
-							</div>
-						</div>
-						{isMobile && <MovieDetails data={data} />}
-						<ProductionCompaniesList
-							productionCompanies={data.production_companies}
-						/>
-						<h2>{t("Additional")}</h2>
-						<ul>
-							<li>
-								<Link to="cast">{t("Cast")}</Link>
-							</li>
-							<li>
-								<Link to="reviews">{t("Reviews")}</Link>
-							</li>
-						</ul>
-						<Outlet context={movieId} />
-					</>
-				)}
-			</FadeContent>
-		</section>
-	);
+  return (
+    <section>
+      <FadeContent>
+        {loading && <p>Loading...</p>}
+        {error && <p>Something went wrong, please try again.</p>}
+        {data && (
+          <>
+            <div className={css.poster}>
+              <div
+                className={css.posterBackdrop}
+                style={{
+                  backgroundImage: `url(https://image.tmdb.org/t/p/w1280${data.backdrop_path})`,
+                }}
+              >
+                <div className={css.posterBackdropGradient}></div>
+                <div className={css.posterDetail}>
+                  <div className={css.posterImgContainer}>
+                    <picture>
+                      <source
+                        srcSet={`https://image.tmdb.org/t/p/w342${data.poster_path} 1x, https://image.tmdb.org/t/p/w780${data.poster_path} 2x`}
+                      />
+                      <img
+                        src={`https://image.tmdb.org/t/p/w342${data.poster_path}`}
+                        alt={`${data.title} poster`}
+                        className={css.posterImg}
+                      />
+                    </picture>
+                  </div>
+                  <div className={css.posterText}>
+                    {!isMobile && <MovieDetails data={data} />}
+                  </div>
+                </div>
+              </div>
+            </div>
+            {isMobile && <MovieDetails data={data} />}
+            <ProductionCompaniesList
+              productionCompanies={data.production_companies}
+            />
+            <h2>{t("Additional")}</h2>
+            <ul>
+              <li>
+                <Link to="cast">{t("Cast")}</Link>
+              </li>
+              <li>
+                <Link to="reviews">{t("Reviews")}</Link>
+              </li>
+            </ul>
+            <Outlet context={movieId} />
+          </>
+        )}
+      </FadeContent>
+    </section>
+  );
 };
 
 export default MovieDetailsPage;

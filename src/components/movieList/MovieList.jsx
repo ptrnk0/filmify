e07de008175633c9649c4a7ba04b5973a269f2b8
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-21 17:49:05 +0200

    feat: implement responsive image handling with picture element in MovieList component

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -3,24 +3,30 @@ import { Link } from "react-router-dom";
 import css from "./MovieList.module.css";
 
 const MovieList = ({ movies }) => {
-	return (
-		<ul className={css.movieList}>
-			{movies.map((movie) => {
-				return (
-					<li key={movie.id} className={css.movieListItem}>
-						<Link to={`/movies/${movie.id}`}>
-							<div className={css.movieImgContainer}>
-								<img
-									src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-									alt={`${movie.title}`}
-								/>
-							</div>
-						</Link>
-					</li>
-				);
-			})}
-		</ul>
-	);
+  return (
+    <ul className={css.movieList}>
+      {movies.map((movie) => {
+        return (
+          <li key={movie.id} className={css.movieListItem}>
+            <Link to={`/movies/${movie.id}`}>
+              <div className={css.movieImgContainer}>
+                <picture>
+                  <source
+                    srcSet={`https://image.tmdb.org/t/p/w342${movie.poster_path} 1x, https://image.tmdb.org/t/p/w780${movie.poster_path} 2x`}
+                  />
+                  <img
+                    src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+                    alt={`${movie.title} poster`}
+                    className={css.posterImg}
+                  />
+                </picture>
+              </div>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default MovieList;

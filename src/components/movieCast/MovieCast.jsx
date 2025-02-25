5d92ee994b110a9ef1c08ce7fd9aef14ceb70744
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-25 16:34:59 +0200

    feat: replace image tags with Avatar component for movie cast display

--- src/components/movieCast/MovieCast.jsx
+++ src/components/movieCast/MovieCast.jsx
@@ -1,66 +1,66 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Scrollbar, Keyboard, Mousewheel } from "swiper/modules";
 import { useOutletContext } from "react-router-dom";
+import { UserOutlined } from "@ant-design/icons";
 
 import css from "./MovieCast.module.css";
 
 import useFetchData from "../../hooks/useFetchData";
+import { Avatar } from "antd";
 
 const MovieCast = () => {
-	const movieId = useOutletContext();
+  const movieId = useOutletContext();
 
-	const { data, loading, error } = useFetchData("credits", movieId);
+  const { data, loading, error } = useFetchData("credits", movieId);
 
-	return (
-		<div className={css.castContainer}>
-			{loading && <p>Loading...</p>}
-			{error && <p>Something went wrong, please try again.</p>}
-			{data && (
-				<Swiper
-					modules={[Scrollbar, Keyboard, Mousewheel]}
-					spaceBetween={20}
-					mousewheel={{
-						invert: false,
-					}}
-					keyboard={{
-						enabled: true,
-						onlyInViewport: true,
-					}}
-					scrollbar={{
-						hide: false,
-					}}
-					slidesPerView={3}
-					className={css.castSwiper}
-				>
-					{data.cast.map((member) => {
-						return (
-							<SwiperSlide
-								key={member.id}
-								className={css.castSlide}
-							>
-								<img
-									src={
-										member.profile_path
-											? `https://image.tmdb.org/t/p/w185${member.profile_path}`
-											: "/download.png"
-									}
-									alt={`${member.name}`}
-								/>
-								<div className={css.castInfo}>
-									<p className={css.castInfoName}>
-										{member.name}
-									</p>
-									<p className={css.castInfoCharacter}>
-										{member.character}
-									</p>
-								</div>
-							</SwiperSlide>
-						);
-					})}
-				</Swiper>
-			)}
-		</div>
-	);
+  return (
+    <div className={css.castContainer}>
+      {loading && <p>Loading...</p>}
+      {error && <p>Something went wrong, please try again.</p>}
+      {data && (
+        <Swiper
+          modules={[Scrollbar, Keyboard, Mousewheel]}
+          spaceBetween={20}
+          mousewheel={{
+            invert: false,
+          }}
+          keyboard={{
+            enabled: true,
+            onlyInViewport: true,
+          }}
+          scrollbar={{
+            hide: false,
+          }}
+          slidesPerView={3}
+          className={css.castSwiper}
+        >
+          {data.cast.map((member) => {
+            return (
+              <SwiperSlide key={member.id} className={css.castSlide}>
+                {member.profile_path ? (
+                  <Avatar
+                    shape="square"
+                    size={79}
+                    src={`https://image.tmdb.org/t/p/w185${member.profile_path}`}
+                  />
+                ) : (
+                  // <img
+                  //   src={`https://image.tmdb.org/t/p/w185${member.profile_path}`}
+                  //   alt={`${member.name}`}
+                  // />
+                  <Avatar shape="square" size={79} icon={<UserOutlined />} />
+                )}
+                <div className={css.castInfo}>
+                  <p className={css.castInfoName}>{member.name}</p>
+                  <p className={css.castInfoCharacter}>{member.character}</p>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      )}
+    </div>
+  );
 };
 
 export default MovieCast;

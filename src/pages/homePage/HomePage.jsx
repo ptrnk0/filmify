acde9078f728338911c143d0603f5dda84904f12
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-25 16:33:40 +0200

    feat: integrate CarouselComponent and add pagination to HomePage

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,16 +1,24 @@
 import useFetchData from "../../hooks/useFetchData";
 import MovieList from "../../components/movieList/MovieList";
+import CarouselComponent from "../../components/carouselComponent/CarouselComponent.jsx";
+import { Pagination } from "antd";
 
 const HomePage = () => {
-	const { data, loading, error } = useFetchData("trendingByDay");
+  const { data, loading, error } = useFetchData("trendingByDay");
 
-	return (
-		<>
-			{loading && <p>Loading...</p>}
-			{error && <p>Something went wrong, please try again.</p>}
-			{data && <MovieList movies={data.results} />}
-		</>
-	);
+  return (
+    <>
+      {loading && <p>Loading...</p>}
+      {error && <p>Something went wrong, please try again.</p>}
+      {data && (
+        <>
+          <CarouselComponent movies={data.results} />
+          <MovieList movies={data.results} />
+          <Pagination defaultCurrent={1} total={50} align="center" />
+        </>
+      )}
+    </>
+  );
 };
 
 export default HomePage;

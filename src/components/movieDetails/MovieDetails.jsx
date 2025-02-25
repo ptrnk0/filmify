e8613fa562c3af699cc933cb7dc244d02496f976
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-25 12:57:01 +0200

    feat: add Ant Design Rate component for user score display in MovieDetails

--- src/components/movieDetails/MovieDetails.jsx
+++ src/components/movieDetails/MovieDetails.jsx
@@ -1,26 +1,29 @@
 import { useTranslation } from "react-i18next";
 
 import css from "./MovieDetails.module.css";
+import { Rate } from "antd";
 
 const MovieDetails = ({ data }) => {
-	const [t] = useTranslation();
+  const [t] = useTranslation();
 
-	return (
-		<div className={css.movieDetailsContainer}>
-			<h1 className={css.movieTitle}>{data.title}</h1>
-			<p className={css.movieGenres}>
-				{data.genres &&
-					data.genres.map((genre) => genre.name).join(", ")}
-			</p>
-			<p className={css.movieUserScore}>
-				User score: {data.vote_average}
-			</p>
-			<div className={css.movieOverview}>
-				<h2>{t("Overview")}</h2>
-				<p>{data.overview}</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className={css.movieDetailsContainer}>
+      <h1 className={css.movieTitle}>{data.title}</h1>
+      <p className={css.movieGenres}>
+        {data.genres && data.genres.map((genre) => genre.name).join(", ")}
+      </p>
+      <Rate
+        disabled
+        allowHalf={true}
+        defaultValue={data.vote_average}
+        count={10}
+      />
+      <div className={css.movieOverview}>
+        <h2>{t("Overview")}</h2>
+        <p>{data.overview}</p>
+      </div>
+    </div>
+  );
 };
 
 export default MovieDetails;

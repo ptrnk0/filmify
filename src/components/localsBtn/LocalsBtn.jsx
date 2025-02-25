774458e5bb56920f228b9b716aa51406cf266bc9
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-25 13:33:12 +0200

    feat: replace language buttons with Ant Design Select component in LocalsBtn

--- src/components/localsBtn/LocalsBtn.jsx
+++ src/components/localsBtn/LocalsBtn.jsx
@@ -1,30 +1,26 @@
 import i18next from "i18next";
+import { Select } from "antd";
 
 import css from "./LocalsBtn.module.css";
 
 import LOCALS from "../../i18n/constants";
 
 const LocalsBtn = () => {
-	return (
-		<div className={css.localsBtnContainer}>
-			<button
-				onClick={() => {
-					i18next.changeLanguage(LOCALS.EN);
-				}}
-				className={css.localsBtn}
-			>
-				EN
-			</button>
-			<button
-				onClick={() => {
-					i18next.changeLanguage(LOCALS.UK);
-				}}
-				className={css.localsBtn}
-			>
-				UA
-			</button>
-		</div>
-	);
+  return (
+    <Select
+      defaultValue={i18next.language}
+      style={{ width: 60 }}
+      onChange={() => {
+        i18next.changeLanguage(
+          i18next.language === "en" ? LOCALS.UK : LOCALS.EN
+        );
+      }}
+      options={[
+        { value: "en", label: "en" },
+        { value: "uk", label: "uk" },
+      ]}
+    />
+  );
 };
 
 export default LocalsBtn;

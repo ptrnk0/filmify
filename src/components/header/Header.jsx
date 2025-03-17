a73eb90e47822da467977146ea700cc1d3305e73
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-17 18:53:39 +0200

    feat: enhance Header component with navigation links and logo integration

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,64 @@
 import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 
-import LocalsBtn from "../localsBtn/LocalsBtn";
+import Logo from "../logo/Logo.jsx";
 
 const Header = () => {
   const [t] = useTranslation();
 
   return (
-    <header>
+    <header className="fixed top-0 w-full p-[20px]">
       <nav>
-        <ul>
+        <ul className="flex flex-row justify-between">
           <li>
-            <NavLink to="/">{t("Trending")}</NavLink>
+            <Logo />
           </li>
-          <li>
-            <NavLink to="/movies">{t("Search")}</NavLink>
-          </li>
+          <div className="flex flex-row items-stretch justify-center gap-[50px] font-medium invert">
+            <li>
+              <NavLink to="/" className="flex h-full items-center">
+                Main
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/" className="flex h-full items-center">
+                Films
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/" className="flex h-full items-center">
+                Series
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/" className="flex h-full items-center">
+                New and popular
+              </NavLink>
+            </li>
+          </div>
+          <div className="flex flex-row items-center justify-center gap-[20px] invert">
+            <li>
+              <NavLink
+                to="/movies"
+                className="flex size-[44px] items-center justify-center rounded-full bg-black/10"
+              >
+                <svg className="size-[24px] fill-none stroke-black">
+                  <use href="/icons.svg#icon-search"></use>
+                </svg>
+              </NavLink>
+            </li>
+            <li className="flex size-[44px] items-center justify-center rounded-full bg-black/10">
+              <svg className="size-[24px] fill-none stroke-black">
+                <use href="/icons.svg#icon-notification"></use>
+              </svg>
+            </li>
+            <li>
+              <svg className="size-[44px] fill-none stroke-black">
+                <use href="/icons.svg#icon-user"></use>
+              </svg>
+            </li>
+          </div>
         </ul>
       </nav>
-      <LocalsBtn />
     </header>
   );
 };

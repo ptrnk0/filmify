cd522095915f605f0749c42cfd6e9c5d8f3433bb
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-17 18:53:47 +0200

    feat: update Logo component styling to use primary text color

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <Link to="/" className="text-5xl uppercase">
+    <Link to="/" className="text-primary text-5xl uppercase">
       Filmify
     </Link>
   );

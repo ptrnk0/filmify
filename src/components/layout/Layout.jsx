db5db8ce0c3708b89de3c7e4b5d4882738d9df31
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-17 13:24:54 +0200

    refactor: simplify Navigation component by removing Logo import and unnecessary div

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,19 +1,17 @@
 import { Outlet } from "react-router-dom";
 
 import Header from "../header/Header";
-import Logo from "../logo/Logo";
 import Footer from "../footer/Footer";
 
 const Navigation = () => {
   return (
-    <div>
-      <Logo />
+    <>
       <Header />
-      <main className="container">
+      <main>
         <Outlet />
       </main>
       <Footer />
-    </div>
+    </>
   );
 };
 

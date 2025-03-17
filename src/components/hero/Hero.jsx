1f14c135bd0d79b145ed75041c40d1b756b38224
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-17 20:01:29 +0200

    feat: enhance Hero component layout and add action buttons for trailer and list

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -9,17 +9,35 @@ const Hero = () => {
     <>
       {data && (
         <section
-          className={`bg-[url(https://image.tmdb.org/t/p/w1280${firstMovie.backdrop_path})] flex h-150 items-center bg-cover bg-center bg-no-repeat`}
+          className={`flex h-150 items-center bg-[url(https://image.tmdb.org/t/p/w1280${firstMovie.backdrop_path})] bg-cover bg-center bg-no-repeat`}
         >
-          <div className="flex flex-col gap-[20px] invert">
-            <h2 className="text-6xl">{firstMovie.title}</h2>
-            <ul className="flex items-center gap-[12px]">
+          <div className="mx-auto flex flex-col gap-[20px] xl:w-10/12">
+            <h2 className="text-6xl invert">{firstMovie.title}</h2>
+            <ul className="flex items-center gap-[12px] font-thin invert">
               <li>{`${firstMovie.vote_average.toFixed(1)}/10 IMDb`}</li>
               <li>{firstMovie.release_date.slice(0, 4)}</li>
               <li></li>
               <li></li>
             </ul>
-            <p className="w-[500px]">{firstMovie.overview}</p>
+            <p className="w-[500px] invert">{firstMovie.overview}</p>
+            <ul className="flex gap-[22px]">
+              <li>
+                <button className="bg-primary flex items-center justify-center gap-[8px] rounded-md px-[12px] py-[8px] text-white">
+                  <svg className="size-[20px] fill-none stroke-white">
+                    <use href="/public/icons.svg#icon-play" />
+                  </svg>
+                  Watch trailer
+                </button>
+              </li>
+              <li>
+                <button className="bg-primary flex items-center justify-center gap-[8px] rounded-md px-[12px] py-[8px] text-white">
+                  <svg className="size-[16px] fill-none stroke-white">
+                    <use href="/public/icons.svg#icon-plus" />
+                  </svg>
+                  Add to List
+                </button>
+              </li>
+            </ul>
           </div>
         </section>
       )}

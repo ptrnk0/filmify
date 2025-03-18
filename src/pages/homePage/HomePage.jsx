6b69768f158c35dcf87add1e46d080fcbf717459
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-18 17:36:41 +0200

    feat: add MovieList component to HomePage for displaying popular trailers

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,7 +1,17 @@
 import Hero from "../../components/hero/Hero.jsx";
+import MovieList from "../../components/movieList/MovieList.jsx";
 
 const HomePage = () => {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <div className="relative -top-24">
+        <MovieList>Popular trailers</MovieList>
+        <MovieList>Popular trailers</MovieList>
+        <MovieList>Popular trailers</MovieList>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;

68fca3009dd6eaf29b54ab48804b8f04183b56c7
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-30 22:04:36 +0300

    feat: allow custom className prop for Logo component

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 
-const Logo = () => {
+const Logo = ({ className }) => {
   return (
-    <Link to="/" className="text-primary text-5xl uppercase">
+    <Link to="/" className={`text-primary text-5xl uppercase ${className}`}>
       Filmify
     </Link>
   );

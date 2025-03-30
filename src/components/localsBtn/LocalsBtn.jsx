b4d974307078cf3489b3b175b8256ce760d2ab7f
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-30 22:04:42 +0300

    feat: enhance LocalsBtn component with language flags and improved layout

--- src/components/localsBtn/LocalsBtn.jsx
+++ src/components/localsBtn/LocalsBtn.jsx
@@ -4,20 +4,23 @@ import LOCALS from "../../i18n/constants";
 
 const LocalsBtn = () => {
   return (
-    <div>
+    <div className="flex gap-1">
       <button
         onClick={() => {
           i18next.changeLanguage(LOCALS.EN);
         }}
+        className="cursor-pointer"
       >
-        EN
+        &#x1F1EC;&#x1F1E7; English
       </button>
+      <span>|</span>
       <button
+        className="cursor-pointer"
         onClick={() => {
           i18next.changeLanguage(LOCALS.UK);
         }}
       >
-        UA
+        &#x1F1FA;&#x1F1E6; Українська
       </button>
     </div>
   );
